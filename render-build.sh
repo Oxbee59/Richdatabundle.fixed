6e4fcbed8a2501b13f3aa1c922163756f5aef855
Oxbee59 <agyeiosborn59@gmail.com>
2025-10-30 22:39:23 +0000

    Added render-build.sh to auto-migrate and collectstatic

---
+++ render-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# render-build.sh
+
+echo "Applying database migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
